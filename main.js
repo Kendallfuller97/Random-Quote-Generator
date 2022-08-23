aab04ef81aac5117fec08abc850aa1e0d38321d9
Kendall Fuller <kendallfuller97@yahoo.com>
2022-08-23 15:36:17 -0700

    Starter Code

---
+++ main.js
@@ -0,0 +1,17 @@
+
+/*** 
+ * `quotes` array 
+***/
+
+
+/***
+ * `getRandomQuote` function
+***/
+
+
+/***
+ * `printQuote` function
+***/
+
+
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
